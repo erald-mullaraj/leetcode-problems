3731517ca1020299a164e98e67dc9d14000ac370
erald-mullaraj <eraldmullaraj@gmail.com>
2023-07-27 13:54:53 +0200

    solution wich modifies the initial input

--- java/Hard/SudokuSolver.java
+++ java/Hard/SudokuSolver.java
@@ -2,16 +2,8 @@ import java.util.Arrays;
 
 public class SudokuSolver {
 
-    interface verification {
-        Boolean verify(int a, int b, int x);
-    }
-    interface findsolution {
-        void findSudoku();
-    }
-    
-
-    public static void main(String[] args) {
-        char[][] mtrx = new char[][]{{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+    static char[][] a = new char[9][9];
+    static char[][] mtrx = new char[][]{{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
                                       {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
                                       {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
                                       {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
@@ -19,18 +11,26 @@ public class SudokuSolver {
                                       {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
                                       {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
                                       {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-                                      {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};    
+                                      {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    public static void main(String[] args) {
+         
         solveSudoku(mtrx);
+        System.out.println(Arrays.deepToString(mtrx));
     }
 
     public static void solveSudoku(char[][] board){
-        verification v = new verification(){
-            public Boolean verify(int a, int b, int x){
+        solve(board);
+        for(int i=0; i<9; i++)
+            for(int j=0; j<9; j++)
+                board[i][j]=a[i][j]; 
+    }
+
+    public static Boolean verify(int a, int b, int x, char[][] boardToVerify){
                 for (int i = 0; i < 9; i++){
-                    if (board[a][i] == (char)x){
+                    if (boardToVerify[a][i] == (char)x){
                         return false;            
                     }
-                    if (board[i][b] == (char)x){
+                    if (boardToVerify[i][b] == (char)x){
                         return false;
                     }
                 }
@@ -38,34 +38,31 @@ public class SudokuSolver {
                 int sqB = (a/3)*3;
                 for (int i = 0; i<3; i++){
                     for (int j = 0; j < 3; j++){
-                        if (board[sqB+i][sqA+j] == x){
+                        if (boardToVerify[sqB+i][sqA+j] == x){
                             return false;
                         }
                     }
                 }
                 return true;
             }
-        };  
 
-        findsolution f = new findsolution(){
-            public void findSudoku(){
-                for (int i = 0; i < 9; i++){
-                    for (int j = 0; j < 9; j++){
-                        if (board[i][j] == '.'){
-                            for (int x = 49; x < 58; x++){
-                                if (v.verify(i, j, x)){
-                                    board[i][j] = (char) x;
-                                    findSudoku();
-                                    board[i][j] = '.';
-                                }
-                            }
-                            return;
+    public static void solve(char[][] boardToSolve){
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (boardToSolve[i][j] == '.'){
+                    for (int x = 49; x < 58; x++){
+                        if (verify(i, j, x, boardToSolve)){
+                            boardToSolve[i][j] = (char) x;
+                            solve(boardToSolve);
+                            boardToSolve[i][j] = '.';
                         }
                     }
+                    return;
                 }
-                System.out.println(Arrays.deepToString(board));   
             }
-        };
-        f.findSudoku();
+        }
+        for(int i=0; i<9; i++)
+            for(int j=0; j<9; j++)
+                a[i][j]=boardToSolve[i][j]; 
     }
 }
