024a8495ca1db0a743e67d01b09c7f4ab4702f7f
erald-mullaraj <eraldmullaraj@gmail.com>
2023-07-31 00:19:47 +0200

    new easy problem

---
+++ java/Easy/MergeTwoSortedLists.java
@@ -0,0 +1,47 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val; }
+    ListNode(int val, ListNode next) {
+         this.val = val; this.next = next; }
+ }
+
+public class MergeTwoSortedLists {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    
+        ListNode start = new ListNode(0);
+        ListNode l3 = start;
+        
+        while (list1 != null && list2 != null){
+            
+                if (list1.val < list2.val) {
+                    ListNode newNode = new ListNode(list1.val);
+                    l3.next = newNode;
+                    l3 = l3.next;
+                    list1 = list1.next;
+                } else {
+                    ListNode newNode = new ListNode(list2.val);
+                    l3.next = newNode;
+                    l3 = l3.next;
+                    list2 = list2.next;
+                }  
+            }
+        while (list1 != null) {
+            ListNode newNode = new ListNode(list1.val);
+            l3.next = newNode;
+            l3 = l3.next;
+            list1 = list1.next;
+            
+        }
+        while (list2 != null) {
+            ListNode newNode = new ListNode(list2.val);
+            l3.next = newNode;
+            l3 = l3.next;
+            list2 = list2.next;
+        }
+        return start.next;
+    }  
+}
