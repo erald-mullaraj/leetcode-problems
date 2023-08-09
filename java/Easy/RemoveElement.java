84fb4d4152ca9375181336a3c5ddda5aa1d89f01
erald-mullaraj <eraldmullaraj@gmail.com>
2023-08-09 23:59:29 +0200

    new easy problem

---
+++ java/Easy/RemoveElement.java
@@ -0,0 +1,28 @@
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int values[] = new int[]{4,4,0,1,0,2};
+        System.out.println(removeElement(values,  0));
+        System.out.println(Arrays.toString(values));
+    }
+
+    private static int removeElement(int[] nums, int val) {
+        int k = 0;
+        int l =nums.length-1;
+        int lend = l;
+        for (int i = 0;i <= l;i++){
+            if (nums[i] == val){
+                k++;
+                for(int j = lend; j > i; j--){
+                    if(nums[j]!=val){
+                        nums[i] = nums[j];
+                        nums[j] = val+1;
+                        lend = j-1;
+                        break;
+                    }
+                }
+            } 
+        }
+        return l+1-k;
+    } 
+}
