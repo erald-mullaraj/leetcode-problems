fca7b5306b2e7ce81d2ed1cb44ec853af30e3d33
erald-mullaraj <eraldmullaraj@gmail.com>
2023-08-21 00:11:09 +0200

    new easy problem

---
+++ java/Easy/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] a = {2,14,18,22,22};
+        System.out.println(containsDuplicateHashSet(a));
+    }
+    public static boolean containsDuplicateBF(int[] nums){
+        int l = nums.length;
+        for (int i = 0;i < l; i ++){
+            for (int j = i+1; j< l;j++){
+                if (nums[i]==nums[j]){
+                    return true;
+                }
+            } 
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicateHashSet(int[] nums){
+        HashSet<Integer> a = new HashSet<Integer>();
+        for (int i = 0;i < nums.length; i ++){
+            if (a.contains(nums[i])){
+                return true;
+            }
+            a.add(nums[i]);
+        }
+        return false;
+    }
+}
