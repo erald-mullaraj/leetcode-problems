a8e4a20423c0e634b4d8098c2b8b599ac3d436b7
erald-mullaraj <eraldmullaraj@gmail.com>
2023-08-26 11:16:23 +0200

    new medium problem

---
+++ java/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        String a = "adal;djf;alksdfj";
+        System.out.println(lengthOfLongestSubstringa(a));
+        ;
+    }
+
+    public static int lengthOfLongestSubstringa(String s) {
+        int[] chIndx = new int[128];
+        int str = 0;
+        int end= 0;
+        int maxL = 0;
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if (chIndx[c] > str){
+                str = chIndx[c];
+            }
+            chIndx[c] = end + 1;
+            maxL = Math.max(maxL, end - str + 1);
+            end++;
+         }
+         return maxL;
+     }
+}
