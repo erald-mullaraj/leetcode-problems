781d4b85c7822560c415016112fe860ea9d5da89
erald-mullaraj <eraldmullaraj@gmail.com>
2023-09-10 20:16:03 +0200

    new easy problem

---
+++ java/Easy/KthLargest.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    int a;
+    public KthLargest(int k, int[] nums) {
+        this.a = k;
+        for(int i : nums){
+            add(i);
+        }
+    }
+    
+    public int add(int val) {
+        if(pq.size() < a){
+            pq.add(val);
+        } else if(val > pq.peek()) {
+            pq.poll();
+            pq.add(val);
+        }
+        return pq.peek();
+    }   
+}
