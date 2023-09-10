9acf4e824c0a16e129f0e936488a084e523e8933
erald-mullaraj <eraldmullaraj@gmail.com>
2023-09-10 23:45:42 +0200

    new easy problem

---
+++ java/Easy/BinaryTreePreorderTraversal.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(Treenode root) {
+        List<Integer> a = new ArrayList<>();
+        recurision(root, a);
+        return a;
+    }
+
+    public void recurision(TreeNode root, List<Integer> lst) {
+        if(root == null){
+            return;
+        }
+        lst.add(root.val);
+        recurision(root.left, lst);
+        recurision(root.right, lst);
+    }
+}
