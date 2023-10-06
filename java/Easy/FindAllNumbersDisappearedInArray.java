4a49e5afa5d41d38a77c5cc610f600b436d0b41f
erald-mullaraj <eraldmullaraj@gmail.com>
2023-10-07 00:36:27 +0200

    update easy problem with fster solution

--- java/Easy/FindAllNumbersDisappearedInArray.java
+++ java/Easy/FindAllNumbersDisappearedInArray.java
@@ -11,8 +11,25 @@ public class FindAllNumbersDisappearedInArray {
         int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
         System.out.println(findDisappearedNumbers(nums));
     }
-    
+
     public static List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> l = new ArrayList<>();
+        int i = 1;
+        int[] a = new int[nums.length];
+        for(int n : nums){
+            a[n-1]++;
+        }
+        while ( i < nums.length){
+            if( a[i] == 0){
+                l.add(i+1);
+            }
+            i++;
+        }
+        return l;
+    }
+    
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
 
         List<Integer> l = new ArrayList<>();
         Set<Integer> a = new HashSet<>(nums.length);
