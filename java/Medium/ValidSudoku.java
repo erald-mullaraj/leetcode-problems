7d0b58ba6bc9516fa3ed859eaf229493b33909f5
erald-mullaraj <eraldmullaraj@gmail.com>
2024-01-15 00:37:11 +0100

    added second optimised solution

--- java/Medium/ValidSudoku.java
+++ java/Medium/ValidSudoku.java
@@ -62,5 +62,36 @@ public class ValidSudoku {
         }
         return true;     
     }
+
+    public static boolean isValidSudoku2(char[][] board) {
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.'){
+                    continue;
+                }
+                for(int x = 0; x < 9; x++){
+                    if(x != i && board[x][j] == board[i][j]){
+                        return false;
+                    }
+                }
+                for(int y = 0; y < 9; y++){
+                    if(y != j && board[i][y] == board[i][j]){
+                        return false;
+                    }
+                }
+                int a = (i/3)*3;
+                int b = (j/3)*3;
+                for (int x = a; x < a + 3; x++) {
+                    for (int y = b; y < b + 3; y++) {
+                        if ((x != i || y != j) && (board[x][y] == board[i][j])){
+                            return false;
+                            }
+                        }
+                    }
+                }
+            }
+        return true;     
+    }
 }
 
