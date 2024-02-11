925a8691da30d62cd5cb54dd338bf9f9bf81a92e
erald-mullaraj <eraldmullaraj@gmail.com>
2024-02-11 17:58:41 +0100

    new fun problem

---
+++ java/Random/Letrat.java
@@ -0,0 +1,36 @@
+package Random;
+
+public class Letrat {
+
+    static double PI = 3.14;
+
+    public static double gjejGjatesine(double innerRadius, double thickness, double height){
+        double gjatesia = 0;
+        for (double r = 0; r < height; r += thickness){
+            gjatesia = gjatesia + PI * 2 * (innerRadius + r + thickness/2);
+        }
+        //pjestojm me 10 per ta kthyer ne cm
+        return gjatesia/10;
+    }
+
+    // per performance me te shpejte pa perdorur loop
+    public static double gjejGjatesine2(double innerRadius, double thickness, double height){
+        double gjatesia = 0;
+        
+        gjatesia =PI * 2 * (height/thickness)* ((innerRadius + thickness/2) + (innerRadius + height - thickness/2))/2;
+            
+        //pjestojm me 10 per ta kthyer ne cm
+        return gjatesia/10;
+    }
+     
+
+    public static void main(String[] args){
+
+        System.out.println(gjejGjatesine(50, 0.5, 1));
+        System.out.println(gjejGjatesine2(50, 0.5, 1));
+        
+
+    }
+
+}
+    
