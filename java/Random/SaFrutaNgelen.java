fdc98e53b947d9b90375a886bae84531eaf0f6cb
erald-mullaraj <eraldmullaraj@gmail.com>
2024-02-19 14:01:25 +0100

    without using set

--- java/Random/SaFrutaNgelen.java
+++ java/Random/SaFrutaNgelen.java
@@ -1,30 +1,26 @@
 package Random;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class SaFrutaNgelen {
     
     public static void main(String[] args){
-        int [] a = {2, 2, 2, 2, 3, 1 , 5, 4};
+        int [] a = {2, 2, 2, 2, 3, 1};
         System.out.println(saNgelen(a));
     }
 
     public static int saNgelen(int[] frutat){
-        Set<Integer> a = new HashSet<>();
+        int f = frutat[0];
         int r = 0;
         for(int i = 0; i < frutat.length; i++){
-            if (a.contains(frutat[i])){
+            if (f == frutat[i]){
                 r++;
-            } else if(!a.contains(frutat[i]) && r == 0){
-                a.add(frutat[i]);
+            } else if(f != frutat[i] && r == 0){
+                f = frutat[i];
                 r++;
-            } else if(!a.contains(frutat[i]) && r == 1){
+            } else if(f != frutat[i] && r == 1){
+                f = 0;
                 r--;
-                a.clear();
             } else {
                 r--;
-                a.remove(frutat[i]);
             }
         }
         return r;
