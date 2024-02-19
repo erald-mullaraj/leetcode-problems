e4bc8842481ce2d57a8e91f1ff7535cbef45aaea
erald-mullaraj <eraldmullaraj@gmail.com>
2024-02-20 00:21:18 +0100

    bug fix

--- kotlin/Random/SaFrutaNgelen.kt
+++ kotlin/Random/SaFrutaNgelen.kt
@@ -6,21 +6,17 @@ fun main (){
 }
 
 fun saFrutaNgelen (frutat: IntArray) : Int{
-    var f = frutat[0]
+    var f = 0
     var r = 0
     for(i in frutat.indices){
         if(f == frutat[i]) r++
-        else if (f != frutat[i]){
-            if(r == 0){
+        else if (f != frutat[i] && r == 0){
                 f = frutat[i]
                 r++
-            }
-            if(r == 1){
+        } else if (f != frutat[i] && r == 1) {
                 f = 0
                 r--
-            }
-            r--;
-        }
+        } else r--;
     }
     return r
 }
