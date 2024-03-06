5562f4b6424fbf68935af5d482414c8257f82e6c
erald-mullaraj <eraldmullaraj@gmail.com>
2024-03-06 23:59:57 +0100

    added second method, much faster than the first one

--- java/Easy/ExcelSheetColumnTitle.java
+++ java/Easy/ExcelSheetColumnTitle.java
@@ -3,23 +3,39 @@ package Easy;
 public class ExcelSheetColumnTitle {
 
     public static void main(String[] args){
-        System.out.println(convertToTitle(28));
+        System.out.println(convertToTitle(703));
+        System.out.println(Solution.convertToTitle(703));
     }
     public static String convertToTitle(int columnNumber) {
-        helper(columnNumber);
-        return str;
+        String st = "";
+        return helper(columnNumber, st);
     }
 
-    static String str = "";
     static String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
-    public static void helper(int nr){
+    public static String helper(int nr, String str){
         if(nr < 27){
             str = s.charAt(nr - 1) + str;
+            return str;
         }
         else {
             str = s.charAt(nr%26 == 0 ? 25 : nr%26 - 1) + str;
-            helper((nr-1)/26);
+            return helper((nr-1)/26, str);
         }
     }
 }
+
+class Solution {
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder r = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            r.insert(0, (char) ((columnNumber % 26) + 'A'));
+            columnNumber /= 26;
+        }
+        
+        return r.toString();        
+    }
+}
+
